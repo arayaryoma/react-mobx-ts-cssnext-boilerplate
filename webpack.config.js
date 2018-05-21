ce32c7c73881b1573ea2fa5a8fc5308f7e58e03a
arayaryoma <r1o.ryoma.abe@gmail.com>
2018-05-21 18:26:13 +0900

    Enable css-modules

--- webpack.config.js
+++ webpack.config.js
@@ -4,58 +4,64 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const config = {
-  resolve: {
-    modules: ["node_modules"],
-    extensions: [".tsx", ".ts", ".js", ".json"]
-  },
-  entry: path.resolve(__dirname, "src/index.tsx"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-    publicPath: "/"
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-      filename: "index.html",
-      minify: {
-        collapseWhitespace: true
-      }
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new OptimizeCSSAssetsPlugin({})
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: "ts-loader"
-      },
-      {
-        test: /\.(css)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "postcss-loader"
-          }
+    resolve: {
+        modules: ["node_modules"],
+        extensions: [".tsx", ".ts", ".js", ".json"]
+    },
+    entry: path.resolve(__dirname, "src/index.tsx"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        publicPath: "/"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html",
+            filename: "index.html",
+            minify: {
+                collapseWhitespace: true
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new OptimizeCSSAssetsPlugin({
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: "ts-loader"
+            },
+            {
+                test: /\.(css)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            localIdentName: "[name]_[local]_[hash:base64:5]",
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: "postcss-loader"
+                    }
+                ]
+            }
         ]
-      }
-    ]
-  },
-  devServer: {
-    port: 4000,
-    historyApiFallback: true,
-    compress: true
-  }
+    },
+    devServer: {
+        port: 4000,
+        historyApiFallback: true,
+        compress: true
+    }
 };
 
 module.exports = config;
